18130b614d105f0e1e044ba4bd3af1a0d4a1d58f
sandrounib <sandrounib@hotmail.com>
2025-09-03 11:27:14 -0300

    inclusão das rotas tela e link para delete e update

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-// var mysql = require('mysql');
-
-// var conecteBanco = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "",
-//     database: "escola"
-// });
-
-// var conexao = require("./conexaoBanco");
-
-// ANTES
-// conecteBanco.connect(function(error){
-//     if(error) throw error;
-//     console.log("O banco de dados foi conectado! ");
-
-// conexao.connect(function(error){
-//     if(error) throw error; 
-
-//     conexao.query("select * from estudante", function(error, result){
-//         if(error) throw error;
-//         console.log(result[0]);
-//         console.log(result[0].nomecompleto);
-//     });
-// });
-
-/*
 var conexao = require('./conexaoBanco');
 
 var express = require('express');
@@ -40,50 +13,33 @@ app.use(bodyParser.urlencoded({ extended: true}));
 //após instalar npm install ejs...
 app.set('view engine', 'ejs');
 
-//CONEXÃO AO BANCO DE DADOS UMA VEZ NO INÍCIO SOMENTE
-//lembra que comentei a linha do post e get? conexao.connect(function(error){
-// vai ficar aqui agora
+
 conexao.connect(function(error){
     if(error){
         console.log("Erro ao conectar ao banco de dados", error);
         process.exit(); //encerrar a conexão caso a conexão falhe
     }
-});  //após isso pode testar e ver se a conexão para de dar erro
-//http://localhost:7000/  e cadastra uma pessoa 
-
+});  
 
 
 app.get('/', function(req,res){
     res.sendFile(__dirname+ '/cadastro.html');
 });
 
-// app.post('/', function(req,res) {
-//     console.log(req.body);
-// })
-
 app.post('/', function(req,res) {
     var nomecompleto = req.body.nomecompleto;
     var email = req.body.email;
     var senha = req.body.senha;
 
-    //novo  retirado do post porque tá abrindo 2 vzes no get e post
-    // conexao.connect(function(error){
-    //  if(error) throw error; 
-
-     //prevenindo SQL Injection
+       //prevenindo SQL Injection
      var sql = "INSERT INTO estudante(nomecompleto, email, senha) VALUES (?, ?, ?)";
      conexao.query(sql, [nomecompleto,email,senha], function(error, result){
-        if(error) throw error;
-
-        //antes sem o redirect
-        //res.send("Estudante cadastrado com sucesso! " + result.insertId);
+        if(error) throw error;      
 
          res.redirect('/estudantes');   //para funcionar vá no browser e digita http://localhost:7000  cadastre uma pessoa e ele vai redirecionar para http://localhost:7000/estudantes com o novo usuário cadastrado
      });
 
     });
-// })
-
 
 //com ejs  leitura do banco de dados
 
@@ -108,7 +64,6 @@ app.get('/estudantes', function(req, res){ //estudante não existe é apenas par
         });
 
     });
-// });
 
 //Roda de Delete
 app.get('/delete-estudante', function(req, res){
@@ -124,7 +79,6 @@ app.get('/delete-estudante', function(req, res){
     });
 });
 
-
 //Rota update
 app.get('/update-estudante', function(req, res){
 
@@ -139,11 +93,30 @@ app.get('/update-estudante', function(req, res){
     });
 });
 
+app.post('/update-estudante', function(req, res){
+    var nomecompleto = req.body.nomecompleto;
+    var email = req.body.email;
+    var senha = req.body.senha;
+    var id = req.body.id;
+
+    var sql = "UPDATE estudante SET nomecompleto=?, email=?, senha=? where id=?";
+
+    var id = req.query.id;
+
+    conexao.query(sql, [nomecompleto, email, senha, id], function(error, result){
+        if(error) console.log(error);
+        res.render(__dirname+'alterarestudantes', {estudante:result});
+        res.redirect('/estudantes')
+    });
+});
+
 app.listen(7000);
 
-*/
 
 
+
+
+/*
 // CÓDIGO DA PROFESSORA
 
 var conexao = require ("./conexaoBanco");
@@ -182,27 +155,22 @@ app.post('/', function(req, res){
     conexao.query(sql, [nomecompleto, email, senha], function(error, result){
         if(error) throw error;
  
-      //  res.send("Estudante cadastro com sucesso! " + result.insertId);
+      //res.send("Estudante cadastro com sucesso! " + result.insertId);
  
       res.redirect('/estudantes');
-    });
- 
- 
-    });
- 
- 
- 
+    }); 
+});
+  
 //Leitura do banco de dados
 app.get('/estudantes', function(req, res){
  
     var sql = "select * from estudante";
     conexao.query(sql, function(error, result){
         if(error) console.log(error);
-       // console.log(result);
+       //console.log(result);
        res.render(__dirname+"/estudantes", {estudante:result});
         });
-    });
- 
+    }); 
  
 //Rota de Delete
 app.get('/delete-estudante', function(req, res){
@@ -214,8 +182,7 @@ app.get('/delete-estudante', function(req, res){
   conexao.query(sql, [id], function(error, result){
         if(error) console.log(error);
  
-        res.redirect('/estudantes');
- 
+        res.redirect('/estudantes'); 
   });
 });
  
@@ -249,5 +216,8 @@ app.post('/update-estudante', function(req, res){
         res.redirect('/estudantes')
     });
 });
+
  
 app.listen(7000);
+
+*/
